42ebe77091525d142aaad834731fb8f398e944ac
pranav223 <88893803+pranav223@users.noreply.github.com>
2023-07-11 23:17:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,113 +1,28 @@
-// Define an array of quiz questions
-const quizQuestions = [
-    {
-      question: "What is the capital of France?",
-      options: ["Paris", "London", "Berlin", "Rome"],
-      correctAnswer: "Paris"
-    },
-    {
-      question: "Which planet is known as the Red Planet?",
-      options: ["Venus", "Mars", "Jupiter", "Saturn"],
-      correctAnswer: "Mars"
-    },
-    {
-      question: "What is the chemical symbol for gold?",
-      options: ["Au", "Ag", "Cu", "Fe"],
-      correctAnswer: "Au"
-    }
-  ];
+function calculateBMI() {
+    var heightInput = document.getElementById("height");
+    var weightInput = document.getElementById("weight");
+    var resultDiv = document.getElementById("result");
   
-  // Variables to track quiz state
-  let currentQuestionIndex = 0;
-  let score = 0;
-  let timeLeft = 30;
-  let timerInterval;
+    var height = parseFloat(heightInput.value);
+    var weight = parseFloat(weightInput.value);
   
-  // Function to start the quiz
-  function startQuiz() {
-    // Hide the start button and display the first question
-    document.getElementById("start-button").style.display = "none";
-    displayQuestion();
-    startTimer();
-  }
-  
-  // Function to display a question and its options
-  function displayQuestion() {
-    const currentQuestion = quizQuestions[currentQuestionIndex];
-    const questionText = document.getElementById("question-text");
-    const answerButtons = document.getElementById("answer-buttons");
-  
-    // Clear previous question and answer options
-    questionText.innerHTML = "";
-    answerButtons.innerHTML = "";
-  
-    // Display the current question
-    questionText.innerHTML = currentQuestion.question;
-  
-    // Create answer buttons for each option
-    currentQuestion.options.forEach(option => {
-      const button = document.createElement("button");
-      button.innerText = option;
-      button.classList.add("answer-button");
-      answerButtons.appendChild(button);
-  
-      // Add click event listener to check the answer
-      button.addEventListener("click", function() {
-        checkAnswer(option);
-      });
-    });
-  }
-  
-  // Function to check the selected answer
-  function checkAnswer(selectedOption) {
-    const currentQuestion = quizQuestions[currentQuestionIndex];
-  
-    // Check if the selected answer is correct
-    if (selectedOption === currentQuestion.correctAnswer) {
-      score++;
+    if (isNaN(height) || isNaN(weight)) {
+      resultDiv.innerHTML = "Please enter valid height and weight.";
+      return;
     }
   
-    // Move to the next question or end the quiz if all questions are answered
-    currentQuestionIndex++;
+    var bmi = weight / ((height / 100) ** 2);
+    var category = "";
   
-    if (currentQuestionIndex < quizQuestions.length) {
-      displayQuestion();
+    if (bmi < 18.5) {
+      category = "Underweight";
+    } else if (bmi < 25) {
+      category = "Normal weight";
+    } else if (bmi < 30) {
+      category = "Overweight";
     } else {
-      endQuiz();
+      category = "Obese";
     }
+  
+    resultDiv.innerHTML = "Your BMI is " + bmi.toFixed(2) + " (" + category + ")";
   }
-  
-  // Function to start the timer
-  function startTimer() {
-    timerInterval = setInterval(function() {
-      timeLeft--;
-  
-      // Update the timer text
-      document.getElementById("timer").textContent = timeLeft;
-  
-      // End the quiz if time runs out
-      if (timeLeft <= 0) {
-        endQuiz();
-      }
-    }, 1000);
-  }
-  
-  // Function to end the quiz
-  function endQuiz() {
-    // Stop the timer
-    clearInterval(timerInterval);
-  
-    // Calculate the score percentage
-    const scorePercentage = (score / quizQuestions.length) * 100;
-  
-    // Display the final score
-    const questionContainer = document.getElementById("question-container");
-    questionContainer.innerHTML = `
-      <h2>Quiz Completed!</h2>
-      <p>Your Score: ${score} out of ${quizQuestions.length}</p>
-      <p>Score Percentage: ${scorePercentage}%</p>
-    `;
-  }
-  
-  // Add event listener to start the quiz when the start button is clicked
-  document.getElementById("start-button").addEventListener("click", startQuiz);
